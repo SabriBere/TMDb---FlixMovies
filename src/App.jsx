e0777f54ab740619083703c33617cfbed4826c63
Sabri <sabrinab786@gmail.com>
2022-08-15 15:40:54 -0300

    preparando cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./index.css";
+import Navbar from './components/Navbar1'
 import NewUser from './commons/NewUser';
 import LogUser from './commons/LogUser';
-import Navbar from './components/Navbar'
 import Header from './components/Header';
 import MyFavorites from './components/MyFavorites';
 import Peliculas from './components/Peliculas'
@@ -10,8 +10,8 @@ import Series from './components/Series'
 import SearchUsers from './components/SearchUsers'
 import Content from './components/Content'
 import SingleMovie from './components/SingleMovie' 
+import { Routes, Route } from 'react-router-dom' 
 import { AuthContextProvider } from './Hooks/AuthContext'
-import { Routes, Route } from 'react-router-dom'
 
 
 
@@ -22,17 +22,15 @@ const App = () => {
             <AuthContextProvider>
                 <Navbar />
                 <Routes>
-                    <Route path='/' element={<Header />} />
-                    <Route path='/search/:value' element={<Content />} />
+                    {/* <Route path='/' element={<Header />} />
+                    <Route path='/search/:value' element={<Content />} /> */}
                     <Route path='/register' element={<NewUser />} />
                     <Route path='/login' element={<LogUser />} />
                     <Route path='/favorites' element={<MyFavorites />} />
                     <Route path='/movies' element={<Peliculas />} />
-                    {/* Para mostrar peliculas de forma individual */}
-                    {/* <Route path='/movies/:select' element={<SingleMovie />} /> */}
                     <Route path='/series' element={<Series />} />
                     <Route path='search/users' element={< SearchUsers />} />
-                </Routes>
+                </Routes> 
             </AuthContextProvider>
         </div>
     )
---
+++ src/components/Navbar1.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/navbar copy.scss'
+
+
+
+const Navbar1 = () => {
+
+
+
+    return (
+        <>
+           <p className='text-light'>Hola soy la Navbar</p> 
+        </>
+    )
+}
+
+export default Navbar1;
